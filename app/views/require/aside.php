7eb09db7bf34a7464da38b55455a233c71b85226
Thiago de Jesus <thiagodejesus86@bol.com.br>
2024-10-20 20:22:59 -0300

    new file:   app/views/require/aside.php 	modified:   assets/css/styles.css 	new file:   assets/css/styles.default.css 	modified:   index.php

---
+++ app/views/require/aside.php
@@ -0,0 +1,38 @@
+
+<aside id="aside">
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+            <a href="">
+                <i class="fa fa-home"></i>
+                <span class="desc">Home</span>
+            </a>
+        </li>
+        <li class="list-group-item open-submenu">
+            <a href="" data-bs-toggle="collapse" data-bs-target="#listaConfiguracoes">
+                <i class="fa fa-sliders"></i>
+                <span class="desc">Configurações</span>
+            </a>
+            <ul class="list-group list-group-flush collapse fade show submenu" id="listaConfiguracoes">
+                <li class="list-group-item">
+                    <a href="">
+                        <i class="fa fa-users"></i>
+                        <span class="desc">Temas</span>
+                    </a>
+                </li>
+                <li class="list-group-item">
+                    <a href="">
+                        <i class="fa fa-cogs"></i>
+                        <span class="desc">Sistema</span>
+                    </a>
+                </li>
+                <li class="list-group-item">
+                    <a href="">
+                        <i class="fa fa-users"></i>
+                        <span class="desc">Usuários</span>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="list-group-item"><a href="">Ajuda</a></li>
+    </ul>
+</aside>
