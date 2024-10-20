0bbc41d3f3e7cfde2f848ad98027fba34a4cb524
Thiago de Jesus <thiagodejesus86@bol.com.br>
2024-10-20 20:29:57 -0300

    modified:   app/views/require/footer.php 	modified:   app/views/require/head.php 	new file:   app/views/require/header.php 	deleted:    app/views/require/menu.php 	modified:   assets/css/styles.css 	modified:   index.php

---
+++ app/views/require/head.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sistema Gestor</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" integrity="sha512-c42qTSw/wPZ3/5LBzD+Bw5f7bSF2oxou6wEb+I/lqeaKV5FDIfMvvRp772y4jcJLKuGUOpbJMdg/BTl50fJYAw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="./assets/css/styles.css" />
+
+</head>
+<body>
+
+    <main id="main">
+        
+        <?php include_once "./app/views/require/header.php"; ?>
+
+        <section id="section">
+
+            <?php include_once "./app/views/require/aside.php"; ?>
+            
+            <section id="content">
+                <!-- content here -->
